b42647d52a97995397d4f8c01c3035781e84bdcb
Teemu Jönkkäri <teemu.jonkkari@gmail.com>
2022-09-24 11:25:41 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,16 +4,22 @@ const parser = require("./libs/ttn-v3/imbuildings-decoder");
 const host = process.env.HOST || "0.0.0.0";
 const port = process.env.PORT || 8080;
 
+const AUTH_TOKEN = process.env.AUTH_TOKEN || "SmartMove";
+
 const logger = process.env.NODE_ENV === "production" ? false : true;
 const fastify = require("fastify")({logger});
+
+fastify.addHook("preHandler", async (request, reply) => {
+  if (request.headers["x-user"] !== AUTH_TOKEN)
+    reply.code(401).send("401 Unauthorized");
+  done();
+});
 
 fastify.get("/", async (_, reply) => {
   reply.code(200).send("200 OK");
 });
 
 fastify.post("/", async (request, reply) => {
-  if (request.headers["X-User"] !== "SmartMove") reply.code(401);
-
   const payload = request.body.DevEUI_uplink.payload_hex;
 
   fastify.log.info(
@@ -21,8 +27,7 @@ fastify.post("/", async (request, reply) => {
   );
 
   if (payload) {
-    const input = {fPort: null, bytes: Buffer.from(payload, "hex")};
-    const parsedData = parser.decode(input);
+    const parsedData = parser.decode({bytes: Buffer.from(payload, "hex")});
 
     if (parsedData) {
       fastify.log.info(parsedData);
